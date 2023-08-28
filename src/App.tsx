ddcc186246b31c798f1c744bbd0fccc1833364da
Sebastian Porling <sebastian.porling@factor10.com>
2023-08-28 17:59:32 +0200

    feat: add context for github client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import {
+  ServiceContextProvider,
+  defaultServices,
+} from "./Context/ServiceContext";
 
 function App() {
-  return <div className="app" data-testid="app"></div>;
+  return (
+    <ServiceContextProvider {...defaultServices}>
+      <div className="app" data-testid="app"></div>
+    </ServiceContextProvider>
+  );
 }
 
 export default App;
---
+++ src/Context/ServiceContext.tsx
@@ -0,0 +1,26 @@
+import { GithubClient, IGithubClient } from "$/Api/GithubClient";
+import React, { PropsWithChildren } from "react";
+
+export interface ServiceProviderProps {
+  githubClient: IGithubClient;
+}
+
+export const defaultServices: ServiceProviderProps = {
+  githubClient: new GithubClient(),
+};
+
+const ServiceContext =
+  React.createContext<ServiceProviderProps>(defaultServices);
+
+export const ServiceContextProvider: React.FC<
+  ServiceProviderProps & PropsWithChildren
+> = (props) => {
+  const { children, ...propsWithoutChildren } = props;
+  return (
+    <ServiceContext.Provider value={propsWithoutChildren}>
+      {children}
+    </ServiceContext.Provider>
+  );
+};
+
+export const userServiceContext = () => React.useContext(ServiceContext);
