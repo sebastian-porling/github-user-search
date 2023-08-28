b4f004c2bca7759b2e035302d89584532d898e24
Sebastian Porling <sebastian.porling@factor10.com>
2023-08-28 21:32:28 +0200

    chore: fix so we can run tests with images

--- jest.config.js
+++ jest.config.js
@@ -17,12 +17,13 @@ const swcConfig = {
 module.exports = {
   preset: 'ts-jest',
   roots: ["<rootDir>/tests"],
-  moduleNameMapper: {
+  transform: {
     "^.+\\.tsx?$": ["@swc/jest", swcConfig],
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
     'jest-transform-stub',
     "^.+\\.(css|less|sass|scss)$": "jest-css-modules-transform",
     "^.+\\.js$": "ts-jest",
+    "^.+\\.png$": "jest-transform-stub",
   },
   testEnvironment: "jsdom",
   moduleNameMapper,
