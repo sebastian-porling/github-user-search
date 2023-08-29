0b09d6ec683e30ad0c5acdeaab8478f5f7092657
Sebastian Porling <sebastian.porling@factor10.com>
2023-08-29 07:45:11 +0200

    chore: add some test utils for elements

--- tests/utils/ElementExtensions.tsx
+++ tests/utils/ElementExtensions.tsx
@@ -1,8 +1,16 @@
+import { fireEvent } from "@testing-library/react";
+import user from "@testing-library/user-event";
+
 export {};
 
 declare global {
   export interface Element {
     getByTestId(testid: string): Element;
+    changeIt(value: string): void;
+    getValue(): string | undefined;
+    pressEnter(): Promise<void>;
+    clickIt(): Promise<void>;
+    isDisabled(): boolean;
   }
 }
 
@@ -13,4 +21,27 @@ Element.prototype.getByTestId = function (testid: string) {
   throw new Error(
     `No element found for selector '${selector}' in HTML:\n${this.innerHTML}`
   );
+};
+
+Element.prototype.changeIt = function (value: string): void {
+  fireEvent.change(this, {
+    target: { value },
+  });
+};
+
+Element.prototype.getValue = function (): string | undefined {
+  return (this as unknown as HTMLInputElement).value;
+};
+
+Element.prototype.pressEnter = async function (): Promise<void> {
+  const element = this as HTMLInputElement;
+  await user.type(element, "[Enter]");
+};
+
+Element.prototype.clickIt = async function (): Promise<void> {
+  await user.click(this);
+};
+
+Element.prototype.isDisabled = function (): boolean {
+  return (this as unknown as HTMLButtonElement).disabled;
 };
